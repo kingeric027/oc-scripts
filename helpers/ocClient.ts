d598eb9ba641b8bd712776aa6002b666e76297b0
Zeeshan <zdawood@four51.com>
2020-09-28 12:06:05 -0500

    :sparkles: Add script to bulk update User permission by salesOrg
    https://four51.atlassian.net/browse/AV-678
    :sparkles: add ability specify oc environment, otherwise default to sandbox.

--- helpers/ocClient.ts
+++ helpers/ocClient.ts
@@ -1,10 +1,43 @@
 import * as OrderCloudSDK from 'ordercloud-javascript-sdk';
-
+export enum OcEnv {
+  Sandbox = 'Sandbox',
+  Staging = 'Staging',
+  Production = 'Production',
+}
 export async function ocClient(
   clientID: string,
-  clientSecret: string
+  clientSecret: string,
+  OcEnv?: string
 ): Promise<typeof OrderCloudSDK> {
-  OrderCloudSDK.Configuration.Set({ timeoutInMilliseconds: 60 * 1000 });
+  let environment;
+  switch (OcEnv) {
+    case 'Sandbox':
+      environment = {
+        baseApiUrl: 'https://sandboxapi.ordercloud.io/v1',
+        baseAuthUrl: 'https://sandboxapi.ordercloud.io/oauth/token',
+        timeoutInMilliseconds: 60 * 1000,
+      };
+    case 'Staging':
+      environment = {
+        baseApiUrl: 'https://stagingapi.ordercloud.io/v1',
+        baseAuthUrl: 'https://stagingapi.ordercloud.io/oauth/token',
+        timeoutInMilliseconds: 60 * 1000,
+      };
+    case 'Production':
+      environment = {
+        baseApiUrl: 'https://api.ordercloud.io/v1',
+        baseAuthUrl: 'https://api.ordercloud.io/oauth/token',
+        timeoutInMilliseconds: 60 * 1000,
+      };
+    default:
+      environment = {
+        baseApiUrl: 'https://sandboxapi.ordercloud.io/v1',
+        baseAuthUrl: 'https://sandboxapi.ordercloud.io/oauth/token',
+        timeoutInMilliseconds: 60 * 1000,
+      };
+  }
+
+  OrderCloudSDK.Configuration.Set(environment);
   const response = await OrderCloudSDK.Auth.ClientCredentials(
     clientSecret,
     clientID,
